e01a9ae4b3374917a3e4fe52255c0e8dd791d437
SK SAHIL <sksahil.contact@gmail.com>
2022-01-21 15:10:52 +0530

    Add FeedbackItem, FeedbackList components and data folder containing dummy data.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import Header from "./components/Header";
+import { useState } from "react";
+import FeedbackList from "./components/FeedbackList";
+import FeedbackData from "./data/FeedbackData";
 
 function App() {
+  let [feedback, setFeedback] = useState(FeedbackData);
+
   return (
     <>
       <Header text="Hello World" />
       <div className="container">
         <h1>My app</h1>
       </div>
+
+      <FeedbackList feedback={feedback} />
     </>
   );
 }
